515b4d0119e44f5e581e93e7a7752b5e614d8aa2
Alisson Ayres <alifoo@Alissons-MacBook-Air.local>
2025-12-03 12:29:59 -0300

    adjustment

--- lua/pigment/init.lua
+++ lua/pigment/init.lua
@@ -19,13 +19,14 @@ function M.colorscheme()
 	vim.o.termguicolors = true
 	vim.g.colors_name = "pigment"
 
+	-- Handle Background setting
 	if config.theme == "dark" then
 		vim.o.background = "dark"
 	else
 		vim.o.background = "light"
 	end
 
-	local colors = palette(config.theme) or palettes.vintage
+	local colors = palettes[config.theme] or palettes.vintage
 	local set = vim.api.nvim_set_hl
 
 	-- Canvas
@@ -78,40 +79,33 @@ function M.colorscheme()
 	set(0, "PreProc", { fg = colors.chinese_orange })
 
 	-- Diagnostics and Git
-	-- Errors
 	set(0, "Error", { fg = colors.russian_red, bold = true })
 	set(0, "DiagnosticError", { fg = colors.russian_red })
 	set(0, "DiagnosticWarn", { fg = colors.chinese_orange })
 	set(0, "DiagnosticInfo", { fg = colors.swiss_blue })
 	set(0, "DiagnosticHint", { fg = colors.irish_green })
 
-	-- Git Signs
 	set(0, "Added", { fg = colors.syrian_green })
 	set(0, "Modified", { fg = colors.scandanavian_yellow })
 	set(0, "Removed", { fg = colors.russian_red })
 
 	-- Treesitter links
-	-- These link the fancy Treesitter groups to the standard ones above
 	set(0, "@variable", { link = "Identifier" })
 	set(0, "@function.builtin", { fg = colors.swiss_blue })
 	set(0, "@property", { fg = colors.french_ochre })
 	set(0, "@constructor", { fg = colors.french_canadian_ochre })
 
 	-- mini.statusline theme
-
-	-- Mode Colors
 	set(0, "MiniStatuslineModeNormal", { fg = colors.english_white, bg = colors.italian_sky, bold = true })
 	set(0, "MiniStatuslineModeInsert", { fg = colors.english_white, bg = colors.irish_green, bold = true })
 	set(0, "MiniStatuslineModeVisual", { fg = colors.english_white, bg = colors.italian_sky, bold = true })
 	set(0, "MiniStatuslineModeReplace", { fg = colors.english_white, bg = colors.russian_red, bold = true })
 	set(0, "MiniStatuslineModeCommand", { fg = colors.english_white, bg = colors.chinese_orange, bold = true })
 
-	-- The main statusline body
 	set(0, "MiniStatuslineDevinfo", { fg = colors.ink_light, bg = colors.paper_light })
 	set(0, "MiniStatuslineFilename", { fg = colors.ink, bg = colors.paper_light })
 	set(0, "MiniStatuslineFileinfo", { fg = colors.ink, bg = colors.paper_light })
 
-	-- Inactive statusline
 	set(0, "MiniStatuslineInactive", { fg = colors.ink_light, bg = colors.paper_dim or "#E2D0B3" })
 end
 
